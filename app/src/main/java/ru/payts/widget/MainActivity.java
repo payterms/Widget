20d0768f0d212bf70776253bb99a41a0b4e6bc92
Michael V. Gritsenko <payterms@mail.ru>
2020-05-25 19:38:44 +0300

    Initial commit

--- app/src/main/java/ru/payts/widget/MainActivity.java
+++ app/src/main/java/ru/payts/widget/MainActivity.java
@@ -2,13 +2,30 @@ package ru.payts.widget;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
 import android.os.Bundle;
+import android.widget.RemoteViews;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppWidgetProvider {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    private final static String ExtraMsg = "msg";
+
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+                                int appWidgetId) {
+
+        CharSequence widgetText = context.getString(R.string.appwidget_text);
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.activity_main);
+        // Здесь обновим текст, будем показывать номер виджета
+        views.setTextViewText(R.id.appwidget_text, String.format("%s - %d", widgetText, appWidgetId));
+        appWidgetManager.updateAppWidget(appWidgetId, views);
     }
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+        }
+    }
+
 }
